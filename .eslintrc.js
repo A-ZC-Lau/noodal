95f1187b4ad45c9786480072792ed24ce14d7dfd
Alexander Lau <alexander.lau@akqa.com>
2021-02-04 17:36:37 +1100

    formatting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,111 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+	"root" : true,
+	"env" : {
+		"browser" : true,
+		"node" : true,
+	},
+	"parser" : "vue-eslint-parser",
+	"extends" : [
+		"@nuxtjs/eslint-config-typescript",
+		"plugin:nuxt/recommended",
+	],
+	"parserOptions" : {
+		"sourceType" : "module",
+		"ecmaVersion" : 2020,
+		"parser" : "babel-eslint",
+	},
+	"plugins" : [
+	],
+	// add your custom rules here
+	"rules" : {
+		"@typescript-eslint/indent" : [
+			"warn",
+			"tab",
+			{
+				"FunctionDeclaration" : {
+					"body" : 1,
+				},
+				"FunctionExpression" : {
+					"body" : 1,
+				},
+				"SwitchCase" : 1,
+			},
+		],
+		"arrow-parens" : [
+			"error",
+			"always",
+		],
+		"brace-style" : [
+			"error",
+			"stroustrup",
+		],
+		"camelcase" : "off",
+		"comma-dangle" : [
+			"error",
+			"always",
+		],
+		"indent" : [
+			"warn",
+			"tab",
+			{
+				"FunctionDeclaration" : {
+					"body" : 1,
+				},
+				"FunctionExpression" : {
+					"body" : 1,
+				},
+				"SwitchCase" : 1,
+			},
+		],
+		"key-spacing" : [
+			"error",
+			{
+				"beforeColon" : true,
+			},
+		],
+		"no-case-declarations" : "off",
+		"no-tabs" : "off",
+		"no-trailing-spaces" : "error",
+		"quotes" : [
+			"warn",
+			"double",
+			{ "allowTemplateLiterals" : true, },
+		],
+		"quote-props" : [
+			"error",
+			"always",
+		],
+		"semi" : [
+			"error",
+			"always",
+		],
+		"space-before-function-paren" : [
+			"error",
+			"always",
+		],
+		"vue/html-indent" : [
+			"error",
+			"tab",
+			{
+				"attribute" : 1,
+				"baseIndent" : 1,
+				"closeBracket" : 0,
+				"alignAttributesVertically" : true,
+				"ignores" : [],
+			},
+		],
+	},
+	"overrides" : [
+		{
+			"files" : [
+				"*.vue",
+			],
+			"rules" : {
+				"vue/script-indent" : [
+					"warn",
+					"tab",
+				],
+			},
+		},
+	],
+};
